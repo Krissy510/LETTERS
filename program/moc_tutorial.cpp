e1525fb55c63bee6b3e827202258108db6392b13
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2021-10-16 17:00:38 +0700

    add moc

---
+++ program/moc_tutorial.cpp
@@ -0,0 +1,382 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'tutorial.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../LETTERS/tutorial.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'tutorial.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Tutorial_t {
+    QByteArrayData data[64];
+    char stringdata0[1002];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Tutorial_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Tutorial_t qt_meta_stringdata_Tutorial = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "Tutorial"
+QT_MOC_LITERAL(1, 9, 11), // "backClicked"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 11), // "slash_sound"
+QT_MOC_LITERAL(4, 34, 11), // "initializer"
+QT_MOC_LITERAL(5, 46, 6), // "setCor"
+QT_MOC_LITERAL(6, 53, 8), // "setInCor"
+QT_MOC_LITERAL(7, 62, 10), // "setBoolAll"
+QT_MOC_LITERAL(8, 73, 7), // "setBool"
+QT_MOC_LITERAL(9, 81, 12), // "setBoolAllBT"
+QT_MOC_LITERAL(10, 94, 11), // "blink_enemy"
+QT_MOC_LITERAL(11, 106, 16), // "func_blink_enemy"
+QT_MOC_LITERAL(12, 123, 12), // "blink_player"
+QT_MOC_LITERAL(13, 136, 17), // "func_blink_player"
+QT_MOC_LITERAL(14, 154, 13), // "update_charbt"
+QT_MOC_LITERAL(15, 168, 13), // "update_anslbl"
+QT_MOC_LITERAL(16, 182, 12), // "update_score"
+QT_MOC_LITERAL(17, 195, 13), // "update_health"
+QT_MOC_LITERAL(18, 209, 11), // "update_item"
+QT_MOC_LITERAL(19, 221, 11), // "reset_vocab"
+QT_MOC_LITERAL(20, 233, 21), // "player_attack_monster"
+QT_MOC_LITERAL(21, 255, 8), // "Monster&"
+QT_MOC_LITERAL(22, 264, 8), // "ot_enemy"
+QT_MOC_LITERAL(23, 273, 6), // "length"
+QT_MOC_LITERAL(24, 280, 2), // "sp"
+QT_MOC_LITERAL(25, 283, 21), // "monster_attack_player"
+QT_MOC_LITERAL(26, 305, 7), // "Player&"
+QT_MOC_LITERAL(27, 313, 9), // "ot_player"
+QT_MOC_LITERAL(28, 323, 8), // "tutoItem"
+QT_MOC_LITERAL(29, 332, 6), // "itemID"
+QT_MOC_LITERAL(30, 339, 9), // "funcCheck"
+QT_MOC_LITERAL(31, 349, 4), // "temp"
+QT_MOC_LITERAL(32, 354, 17), // "on_useMax_clicked"
+QT_MOC_LITERAL(33, 372, 20), // "on_usePotion_clicked"
+QT_MOC_LITERAL(34, 393, 17), // "on_useMul_clicked"
+QT_MOC_LITERAL(35, 411, 21), // "on_backButton_clicked"
+QT_MOC_LITERAL(36, 433, 16), // "on_atkbt_clicked"
+QT_MOC_LITERAL(37, 450, 16), // "on_clrbt_clicked"
+QT_MOC_LITERAL(38, 467, 17), // "on_chrbt0_clicked"
+QT_MOC_LITERAL(39, 485, 19), // "on_chrbt0_2_clicked"
+QT_MOC_LITERAL(40, 505, 19), // "on_chrbt0_3_clicked"
+QT_MOC_LITERAL(41, 525, 19), // "on_chrbt0_4_clicked"
+QT_MOC_LITERAL(42, 545, 19), // "on_chrbt0_5_clicked"
+QT_MOC_LITERAL(43, 565, 19), // "on_chrbt0_6_clicked"
+QT_MOC_LITERAL(44, 585, 19), // "on_chrbt0_7_clicked"
+QT_MOC_LITERAL(45, 605, 19), // "on_chrbt0_8_clicked"
+QT_MOC_LITERAL(46, 625, 19), // "on_chrbt0_9_clicked"
+QT_MOC_LITERAL(47, 645, 20), // "on_chrbt0_10_clicked"
+QT_MOC_LITERAL(48, 666, 20), // "on_chrbt0_11_clicked"
+QT_MOC_LITERAL(49, 687, 20), // "on_chrbt0_12_clicked"
+QT_MOC_LITERAL(50, 708, 20), // "on_chrbt0_13_clicked"
+QT_MOC_LITERAL(51, 729, 20), // "on_chrbt0_14_clicked"
+QT_MOC_LITERAL(52, 750, 20), // "on_chrbt0_15_clicked"
+QT_MOC_LITERAL(53, 771, 20), // "on_chrbt0_16_clicked"
+QT_MOC_LITERAL(54, 792, 20), // "on_chrbt0_17_clicked"
+QT_MOC_LITERAL(55, 813, 20), // "on_chrbt0_18_clicked"
+QT_MOC_LITERAL(56, 834, 20), // "on_chrbt0_19_clicked"
+QT_MOC_LITERAL(57, 855, 20), // "on_chrbt0_20_clicked"
+QT_MOC_LITERAL(58, 876, 20), // "on_chrbt0_21_clicked"
+QT_MOC_LITERAL(59, 897, 20), // "on_chrbt0_22_clicked"
+QT_MOC_LITERAL(60, 918, 20), // "on_chrbt0_23_clicked"
+QT_MOC_LITERAL(61, 939, 20), // "on_chrbt0_24_clicked"
+QT_MOC_LITERAL(62, 960, 17), // "on_nextBt_clicked"
+QT_MOC_LITERAL(63, 978, 23) // "on_backButton_2_clicked"
+
+    },
+    "Tutorial\0backClicked\0\0slash_sound\0"
+    "initializer\0setCor\0setInCor\0setBoolAll\0"
+    "setBool\0setBoolAllBT\0blink_enemy\0"
+    "func_blink_enemy\0blink_player\0"
+    "func_blink_player\0update_charbt\0"
+    "update_anslbl\0update_score\0update_health\0"
+    "update_item\0reset_vocab\0player_attack_monster\0"
+    "Monster&\0ot_enemy\0length\0sp\0"
+    "monster_attack_player\0Player&\0ot_player\0"
+    "tutoItem\0itemID\0funcCheck\0temp\0"
+    "on_useMax_clicked\0on_usePotion_clicked\0"
+    "on_useMul_clicked\0on_backButton_clicked\0"
+    "on_atkbt_clicked\0on_clrbt_clicked\0"
+    "on_chrbt0_clicked\0on_chrbt0_2_clicked\0"
+    "on_chrbt0_3_clicked\0on_chrbt0_4_clicked\0"
+    "on_chrbt0_5_clicked\0on_chrbt0_6_clicked\0"
+    "on_chrbt0_7_clicked\0on_chrbt0_8_clicked\0"
+    "on_chrbt0_9_clicked\0on_chrbt0_10_clicked\0"
+    "on_chrbt0_11_clicked\0on_chrbt0_12_clicked\0"
+    "on_chrbt0_13_clicked\0on_chrbt0_14_clicked\0"
+    "on_chrbt0_15_clicked\0on_chrbt0_16_clicked\0"
+    "on_chrbt0_17_clicked\0on_chrbt0_18_clicked\0"
+    "on_chrbt0_19_clicked\0on_chrbt0_20_clicked\0"
+    "on_chrbt0_21_clicked\0on_chrbt0_22_clicked\0"
+    "on_chrbt0_23_clicked\0on_chrbt0_24_clicked\0"
+    "on_nextBt_clicked\0on_backButton_2_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Tutorial[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      54,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  284,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,  285,    2, 0x08 /* Private */,
+       4,    0,  286,    2, 0x08 /* Private */,
+       5,    0,  287,    2, 0x08 /* Private */,
+       6,    0,  288,    2, 0x08 /* Private */,
+       7,    1,  289,    2, 0x08 /* Private */,
+       9,    1,  292,    2, 0x08 /* Private */,
+      10,    0,  295,    2, 0x08 /* Private */,
+      11,    0,  296,    2, 0x08 /* Private */,
+      12,    0,  297,    2, 0x08 /* Private */,
+      13,    0,  298,    2, 0x08 /* Private */,
+      14,    0,  299,    2, 0x08 /* Private */,
+      15,    0,  300,    2, 0x08 /* Private */,
+      16,    0,  301,    2, 0x08 /* Private */,
+      17,    0,  302,    2, 0x08 /* Private */,
+      18,    0,  303,    2, 0x08 /* Private */,
+      19,    0,  304,    2, 0x08 /* Private */,
+      20,    1,  305,    2, 0x08 /* Private */,
+      20,    3,  308,    2, 0x08 /* Private */,
+      25,    1,  315,    2, 0x08 /* Private */,
+      28,    1,  318,    2, 0x08 /* Private */,
+      30,    1,  321,    2, 0x08 /* Private */,
+      32,    0,  324,    2, 0x08 /* Private */,
+      33,    0,  325,    2, 0x08 /* Private */,
+      34,    0,  326,    2, 0x08 /* Private */,
+      35,    0,  327,    2, 0x08 /* Private */,
+      36,    0,  328,    2, 0x08 /* Private */,
+      37,    0,  329,    2, 0x08 /* Private */,
+      38,    0,  330,    2, 0x08 /* Private */,
+      39,    0,  331,    2, 0x08 /* Private */,
+      40,    0,  332,    2, 0x08 /* Private */,
+      41,    0,  333,    2, 0x08 /* Private */,
+      42,    0,  334,    2, 0x08 /* Private */,
+      43,    0,  335,    2, 0x08 /* Private */,
+      44,    0,  336,    2, 0x08 /* Private */,
+      45,    0,  337,    2, 0x08 /* Private */,
+      46,    0,  338,    2, 0x08 /* Private */,
+      47,    0,  339,    2, 0x08 /* Private */,
+      48,    0,  340,    2, 0x08 /* Private */,
+      49,    0,  341,    2, 0x08 /* Private */,
+      50,    0,  342,    2, 0x08 /* Private */,
+      51,    0,  343,    2, 0x08 /* Private */,
+      52,    0,  344,    2, 0x08 /* Private */,
+      53,    0,  345,    2, 0x08 /* Private */,
+      54,    0,  346,    2, 0x08 /* Private */,
+      55,    0,  347,    2, 0x08 /* Private */,
+      56,    0,  348,    2, 0x08 /* Private */,
+      57,    0,  349,    2, 0x08 /* Private */,
+      58,    0,  350,    2, 0x08 /* Private */,
+      59,    0,  351,    2, 0x08 /* Private */,
+      60,    0,  352,    2, 0x08 /* Private */,
+      61,    0,  353,    2, 0x08 /* Private */,
+      62,    0,  354,    2, 0x08 /* Private */,
+      63,    0,  355,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 21,   22,
+    QMetaType::Void, 0x80000000 | 21, QMetaType::Int, QMetaType::Int,   22,   23,   24,
+    QMetaType::Void, 0x80000000 | 26,   27,
+    QMetaType::Void, QMetaType::Int,   29,
+    QMetaType::Void, QMetaType::QString,   31,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Tutorial::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Tutorial *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->backClicked(); break;
+        case 1: _t->slash_sound(); break;
+        case 2: _t->initializer(); break;
+        case 3: _t->setCor(); break;
+        case 4: _t->setInCor(); break;
+        case 5: _t->setBoolAll((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->setBoolAllBT((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->blink_enemy(); break;
+        case 8: _t->func_blink_enemy(); break;
+        case 9: _t->blink_player(); break;
+        case 10: _t->func_blink_player(); break;
+        case 11: _t->update_charbt(); break;
+        case 12: _t->update_anslbl(); break;
+        case 13: _t->update_score(); break;
+        case 14: _t->update_health(); break;
+        case 15: _t->update_item(); break;
+        case 16: _t->reset_vocab(); break;
+        case 17: _t->player_attack_monster((*reinterpret_cast< Monster(*)>(_a[1]))); break;
+        case 18: _t->player_attack_monster((*reinterpret_cast< Monster(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 19: _t->monster_attack_player((*reinterpret_cast< Player(*)>(_a[1]))); break;
+        case 20: _t->tutoItem((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 21: _t->funcCheck((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 22: _t->on_useMax_clicked(); break;
+        case 23: _t->on_usePotion_clicked(); break;
+        case 24: _t->on_useMul_clicked(); break;
+        case 25: _t->on_backButton_clicked(); break;
+        case 26: _t->on_atkbt_clicked(); break;
+        case 27: _t->on_clrbt_clicked(); break;
+        case 28: _t->on_chrbt0_clicked(); break;
+        case 29: _t->on_chrbt0_2_clicked(); break;
+        case 30: _t->on_chrbt0_3_clicked(); break;
+        case 31: _t->on_chrbt0_4_clicked(); break;
+        case 32: _t->on_chrbt0_5_clicked(); break;
+        case 33: _t->on_chrbt0_6_clicked(); break;
+        case 34: _t->on_chrbt0_7_clicked(); break;
+        case 35: _t->on_chrbt0_8_clicked(); break;
+        case 36: _t->on_chrbt0_9_clicked(); break;
+        case 37: _t->on_chrbt0_10_clicked(); break;
+        case 38: _t->on_chrbt0_11_clicked(); break;
+        case 39: _t->on_chrbt0_12_clicked(); break;
+        case 40: _t->on_chrbt0_13_clicked(); break;
+        case 41: _t->on_chrbt0_14_clicked(); break;
+        case 42: _t->on_chrbt0_15_clicked(); break;
+        case 43: _t->on_chrbt0_16_clicked(); break;
+        case 44: _t->on_chrbt0_17_clicked(); break;
+        case 45: _t->on_chrbt0_18_clicked(); break;
+        case 46: _t->on_chrbt0_19_clicked(); break;
+        case 47: _t->on_chrbt0_20_clicked(); break;
+        case 48: _t->on_chrbt0_21_clicked(); break;
+        case 49: _t->on_chrbt0_22_clicked(); break;
+        case 50: _t->on_chrbt0_23_clicked(); break;
+        case 51: _t->on_chrbt0_24_clicked(); break;
+        case 52: _t->on_nextBt_clicked(); break;
+        case 53: _t->on_backButton_2_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Tutorial::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Tutorial::backClicked)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Tutorial::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_Tutorial.data,
+    qt_meta_data_Tutorial,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Tutorial::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Tutorial::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Tutorial.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Tutorial::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 54)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 54;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 54)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 54;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Tutorial::backClicked()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
